171bde04f6146d8b100e50c62d1d95d996b90b1f
AutumnSprite <jguanxp2003@gmail.com>
2026-08-28 23:46:52 +0900

    Add tab bar icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,30 @@
 import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function TabsLayout() {
 	return (
 		<Tabs screenOptions={{ headerShown: false }}>
-			<Tabs.Screen name="index" options={{ title: "Decks" }} />
-			<Tabs.Screen name="review" options={{title: "Review"}} />
-			<Tabs.Screen name="learned" options={{ title: "Learned" }} />
+			<Tabs.Screen
+				name="index"
+				options={{
+				title: "Decks",
+				tabBarIcon: ({ color, size }) => <Ionicons name="albums" color={color} size={size} />,
+				}}
+			/>
+			<Tabs.Screen
+				name="review"
+				options={{
+				title: "Review",
+				tabBarIcon: ({ color, size }) => <Ionicons name="refresh" color={color} size={size} />,
+				}}
+			/>
+			<Tabs.Screen
+				name="learned"
+				options={{
+				title: "Learned",
+				tabBarIcon: ({ color, size }) => <Ionicons name="checkmark-done" color={color} size={size} />,
+				}}
+			/>
 		</Tabs>
 	);
 }
